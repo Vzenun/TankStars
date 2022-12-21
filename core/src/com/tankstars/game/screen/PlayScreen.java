daa81304b2b52d9202826ba9da703bf1529a3642
Vidur <vidur21364@iiitd.ac.in>
2022-12-21 09:25:01 +0530

    Commit

--- core/src/com/tankstars/game/screen/PlayScreen.java
+++ core/src/com/tankstars/game/screen/PlayScreen.java
@@ -8,8 +8,10 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Circle;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector;
 import com.badlogic.gdx.math.Vector2;
@@ -20,7 +22,9 @@ import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.joints.RevoluteJoint;
 import com.badlogic.gdx.physics.box2d.joints.RevoluteJointDef;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.tankstars.game.TankStars;
 import static com.tankstars.game.screen.Constants.PPM;
@@ -32,11 +36,14 @@ import java.io.Serializable;
 
 
 public class PlayScreen implements Screen, InputProcessor, Serializable {
-    boolean turn=false;//false pe Player1 ki turn hai and true pe Player2 ki.
     boolean movingright=false;
     boolean movingleft=false;
     boolean movingup=false;
     boolean movingdown=false;
+    boolean movingright1=false;
+    boolean movingleft1=false;
+    boolean movingup1=false;
+    boolean movingdown1=false;
     private TankStars game;
     private Box2DDebugRenderer b2dr;
     private World world;
@@ -45,7 +52,14 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
     private int tankheight;
     private int tankwidth;
     private Texture tex;private Texture tex2;private Texture tex3;
-    private Body player1,player2,nalli1,nalli2;
+    private Body player1,player2,player3,player11,player12,player13;
+    private RevoluteJoint rwheel,lwheel;
+    private RevoluteJoint rwheel1,lwheel1;
+    private Sprite sp;
+    private Sprite sp2;
+    RevoluteJointDef ref;
+    RevoluteJointDef ref1;
+    private Array<Body> tmpbody=new Array<Body>();
     public PlayScreen(TankStars game){
         this.game=game;
         world=new World(new Vector2(0,-2.8f),true);
@@ -55,11 +69,52 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
         camera.setToOrtho(false,Gdx.graphics.getWidth()/2,Gdx.graphics.getHeight()/2);
         tankheight=20;
         tankwidth=40;
+
         //tex=new Texture("theme11.png");
         tex2=new Texture("terrain1.png");
-        //tex3=new Texture("tank2.png");
-        player1=createTank(false,-200,50,true,20,20);
-        player2=createTank(false,100,50,true,20,20);
+        sp2=new Sprite(tex2);
+        sp2.setPosition(-10.552f,-5.1f);
+        sp2.setSize(640/1/PPM,200/1/PPM);
+        sp2.setOrigin(-400,5000);
+        tex3=new Texture("tank2.png");
+        sp=new Sprite(tex3);
+        player1=createTank(false,-200,50,true,30,30);
+        player2=createwheels(false,-200,50,true,20,20);
+        player3=createwheels(false,-190,50,true,20,20);
+
+        player11=createTank(false,100,50,true,30,30);
+        player12=createwheels(false,100,50,true,20,20);
+        player13=createwheels(false,90,50,true,20,20);
+
+
+        ref = new RevoluteJointDef () ;
+        ref.enableMotor=true;
+        ref.motorSpeed=1;
+        ref.maxMotorTorque=0;
+        ref.bodyA=player1;
+        ref.bodyB=player2;
+        ref.collideConnected=false;
+        ref.localAnchorA.set(-9/PPM,-14/PPM);
+        lwheel=(RevoluteJoint) world.createJoint(ref);
+
+        ref.bodyB=player3;
+        ref.localAnchorA.set(9/PPM,-14/PPM);
+        rwheel=(RevoluteJoint) world.createJoint(ref);
+
+
+        ref1 = new RevoluteJointDef () ;
+        ref1.enableMotor=true;
+        ref1.motorSpeed=1;
+        ref1.maxMotorTorque=0;
+        ref1.bodyA=player11;
+        ref1.bodyB=player12;
+        ref1.collideConnected=false;
+        ref1.localAnchorA.set(-7/PPM,-14/PPM);
+        lwheel1=(RevoluteJoint) world.createJoint(ref1);
+
+        ref1.bodyB=player13;
+        ref1.localAnchorA.set(7/PPM,-14/PPM);
+        rwheel1=(RevoluteJoint) world.createJoint(ref1);
         Gdx.input.setInputProcessor(this);
     }
     @Override
@@ -73,10 +128,20 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
         Gdx.gl.glClearColor(0f,0f,0f,1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         b2dr.render(world, camera.combined.scl(PPM));
+        game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
+        world.getBodies(tmpbody);
         //game.batch.draw(tex,0,0);
-        game.batch.draw(tex2,0,5);
         //game.batch.draw(tex3,player1.getPosition().x*PPM,player1.getPosition().y*PPM);
+        for(Body body : tmpbody){
+            if(body.getUserData()!=null && body.getUserData() instanceof Sprite){
+                Sprite sprite=(Sprite) body.getUserData();
+                sprite.setPosition(body.getPosition().x-sprite.getWidth()/2,body.getPosition().y- sprite.getHeight()/2);
+                sprite.setRotation(body.getAngle()* MathUtils.radiansToDegrees);
+                sprite.draw(game.batch);
+            }
+        }
+        sp2.draw(game.batch);
         //game.batch.draw(tex3,player2.getPosition().x*PPM,player2.getPosition().y*PPM);
         game.batch.end();
     }
@@ -102,15 +167,31 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
     }
     public void inputUpdate(float delta)
     {
-        if(movingleft==true){
-            player1.setLinearVelocity(-0.4f,player1.getLinearVelocity().y);
+        if(movingleft==true) {
+            player2.setLinearVelocity(-0.46f,player2.getLinearVelocity().y);
+            player3.setLinearVelocity(-0.46f,player3.getLinearVelocity().y);
         }
-        else if(movingright==true)
+        if(movingleft1==true) {
+            player12.setLinearVelocity(-0.46f,player12.getLinearVelocity().y);
+            player13.setLinearVelocity(-0.46f,player13.getLinearVelocity().y);
+        }
+        if(movingright==true)
         {
-            player1.setLinearVelocity(0.4f,player1.getLinearVelocity().y);
+            player2.setLinearVelocity(0.46f,player2.getLinearVelocity().y);
+            player3.setLinearVelocity(0.46f,player3.getLinearVelocity().y);
         }
-        else{
-            player1.setLinearVelocity(0,player1.getLinearVelocity().y);
+        if(movingright1==true)
+        {
+            player12.setLinearVelocity(0.46f,player12.getLinearVelocity().y);
+            player13.setLinearVelocity(0.46f,player13.getLinearVelocity().y);
+        }
+        if(movingright1!=true && movingright!=true && movingleft!=true && movingleft1!=true){
+            player2.setLinearVelocity(0,player2.getLinearVelocity().y);
+            player3.setLinearVelocity(0,player3.getLinearVelocity().y);
+            player12.setLinearVelocity(0,player12.getLinearVelocity().y);
+            player13.setLinearVelocity(0,player13.getLinearVelocity().y);
+            //player2.applyForceToCenter(new Vector2(0,2.8f),true);
+            //player3.applyForceToCenter(new Vector2(0,2.8f),true);
         }
     }
     public void cameraUpdate(float delta)
@@ -146,6 +227,18 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
         if (keycode == Input.Keys.DOWN) {
             movingdown = true;
         }
+        if(keycode==Input.Keys.A) {
+            movingleft1=true;
+        }
+        if(keycode==Input.Keys.D){
+            movingright1=true;
+        }
+        if(keycode==Input.Keys.W){
+            movingup1=true;
+        }
+        if(keycode==Input.Keys.S){
+            movingdown1=true;
+        }
         return false;
     }
 
@@ -162,6 +255,18 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
         }
         if(keycode==Input.Keys.DOWN){
             movingdown=false;
+        }
+        if(keycode==Input.Keys.A) {
+            movingleft1=false;
+        }
+        if(keycode==Input.Keys.D){
+            movingright1=false;
+        }
+        if(keycode==Input.Keys.W){
+            movingup1=false;
+        }
+        if(keycode==Input.Keys.S){
+            movingdown1=false;
         }
         return false;
     }
@@ -202,12 +307,12 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
         def.type= BodyDef.BodyType.StaticBody;
 
         ChainShape chain=new ChainShape();
-        chain.createChain(new Vector2[] {new Vector2(-10.57f,0.23f),new Vector2(-8.8f,0.23f),new Vector2(-7.85f,-1.9f),new Vector2(-5.2f,-1.95f),new Vector2(-4.5f,-1.04f),new Vector2(-2.9f,-0.91f),new Vector2(-0.23f,-1.05f),new Vector2(0.7f,-3.02f),new Vector2(3.5f,-3.02f),new Vector2(4.15f,-1.4f),new Vector2(8.1f,-0.95f),new Vector2(8.5f,-1.18f),new Vector2(8.95f,-1.18f),new Vector2(9.24f,-0.35f),new Vector2(9.24f,6.04f),new Vector2(-10.57f,6.04f),new Vector2(-10.57f,0.23f)});
+        chain.createChain(new Vector2[] {new Vector2(-10.57f,0.23f),new Vector2(-8.8f,0.23f),new Vector2(-7.85f,-1.85f),new Vector2(-5.2f,-1.90f),new Vector2(-4.5f,-1.04f),new Vector2(-2.9f,-0.91f),new Vector2(-0.23f,-1.05f),new Vector2(0.7f,-3.02f),new Vector2(3.5f,-3.02f),new Vector2(4.15f,-1.4f),new Vector2(8.1f,-0.95f),new Vector2(8.5f,-1.18f),new Vector2(8.95f,-1.18f),new Vector2(9.24f,-0.35f),new Vector2(9.24f,6.04f),new Vector2(-10.57f,6.04f),new Vector2(-10.57f,0.23f)});
 
         FixtureDef fix=new FixtureDef();
         fix.shape=chain;
         fix.friction=0.5f;
-        fix.restitution=0;
+        fix.restitution=0f;
         body=world.createBody(def);
         body.createFixture(fix);
         return body;
@@ -229,46 +334,66 @@ public class PlayScreen implements Screen, InputProcessor, Serializable {
             def.fixedRotation=false;
         }
         PolygonShape shape=new PolygonShape();
-        shape.setAsBox(tankwidth/2/PPM,tankheight/2/PPM);
+        shape.setAsBox(tankwidth/4/PPM,tankheight/6/PPM);
 
         FixtureDef fix=new FixtureDef();
         fix.shape=shape;
         fix.friction=.05f;
         fix.restitution=.02f;
-        fix.density=7f;
+        fix.density=2.7f;
 
         tbody=world.createBody(def);
 
         tbody.createFixture(fix);
+
+        CircleShape shape1=new CircleShape();
+        shape1.setRadius(.2f);
+
+        FixtureDef fix1=new FixtureDef();
+        fix1.shape=shape1;
+        fix1.friction=.25f;
+        fix1.restitution=.02f;
+        fix1.density=0f;
+
+        tbody.createFixture(fix1);
+        sp.setSize(tankwidth/1/PPM+20/1/PPM,tankheight/1/PPM+10/1/PPM);
+        sp.setOrigin(sp.getWidth()/2,sp.getHeight()/2);
+        tbody.setUserData(sp);
+
+        shape1.dispose();
         shape.dispose();
         return tbody;
     }
-    public Body createNalli(boolean isStatic,int x,int y,boolean isRotatable,int nalliwidth,int nalliheight)
+    public Body createwheels(boolean isStatic,int x,int y,boolean isRotatable,int tankwidth,int tankheight)
     {
         Body tbody;
         BodyDef def=new BodyDef();
-        if(!isStatic)
-        {
+        if(!isStatic){
             def.type=BodyDef.BodyType.DynamicBody;
         }
-        else
-        {
+        else{
             def.type=BodyDef.BodyType.StaticBody;
         }
-        def.position.set(x,y);
-        if(!isRotatable)
-        {
+        def.position.set(x/PPM,y/PPM);
+        if(!isRotatable){
             def.fixedRotation=true;
         }
-        else
-        {
+        else{
             def.fixedRotation=false;
         }
+        CircleShape shape1=new CircleShape();
+        shape1.setRadius(.19f);
+
+        FixtureDef fix1=new FixtureDef();
+        fix1.shape=shape1;
+        fix1.friction=10.25f;
+        fix1.restitution=.02f;
+        fix1.density=18f;
+
         tbody=world.createBody(def);
-        PolygonShape shape=new PolygonShape();
-        shape.setAsBox(nalliwidth/2,nalliheight/2);
-        tbody.createFixture(shape,1.0f);
-        shape.dispose();
+        tbody.createFixture(fix1);
+
+        shape1.dispose();
         return tbody;
     }
 }
